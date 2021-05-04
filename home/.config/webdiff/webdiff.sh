ebafad973210860e72206226ade79215444f6ae6
berrabe@manjaro <rizalical250@gmail.com>
2021-05-04 12:06:44 +0700

    1 - update webdiff config   - fix typo

--- home/.config/webdiff/webdiff.sh
+++ home/.config/webdiff/webdiff.sh
@@ -3,4 +3,4 @@
 # Look at {test.sh,testing.cfg} on https://github.com/danvk/webdiff
 export WEBDIFF_CONFIG=$HOME/.config/webdiff/berrabe.cfg
 export WEBDIFF_PORT=9000
-$(which git-webdiff) $*
+$(which webdiff) $*
